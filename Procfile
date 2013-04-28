12cde57b8a2c2b4ad8e270413633de178bd40f84
Pavlos Gi <raiden4589@gmail.com>
2013-04-28 21:55:59 +0100

    Removed heroku specific

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn bii.wsgi:application
--- deploy/deploy.bat
+++
@@ -1,2 +0,0 @@
-git --git-dir="C:/Users/Pavlos/dropbox/masters/project/workspace/bitbucket/bii/deploy/bii/.git" pull origin master
-git --git-dir="C:/Users/Pavlos/dropbox/masters/project/workspace/bitbucket/bii/deploy/bii/.git" push heroku master
