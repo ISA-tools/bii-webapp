67097f8169ac95d66f8c62a32ec7c1f2fe3df02d
Pavlos <raiden4589@gmail.com>
2013-04-28 13:22:59 +0100

    refined settings to include static

--- bii/settings.py
+++ bii/settings.py
@@ -122,17 +122,14 @@ STATICFILES_FINDERS = (
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL ='/bii_web/static/'
 # Additional locations of static files
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
-
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR,'bii_web/static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
