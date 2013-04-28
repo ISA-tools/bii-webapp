fd402930fd95cbf3e36af4d4a5438e8277173b2b
Pavlos <raiden4589@gmail.com>
2013-04-28 17:50:57 +0100

    refined settings for heroku

--- bii/settings.py
+++ bii/settings.py
@@ -119,11 +119,11 @@ STATICFILES_FINDERS = (
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname( __file__)), "bii_web\static")
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname( __file__)), "bii_web/static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL ='/static/'
+STATIC_URL ='/bii_web/static/'
 # Additional locations of static files
 
 STATICFILES_DIRS = (
