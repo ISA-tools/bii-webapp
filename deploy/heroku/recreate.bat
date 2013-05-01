20f3e01cafaf310b9d6adcfd9795431b2d0a91fd
Pavlos Gi <raiden4589@gmail.com>
2013-05-01 23:48:50 +0100

    changed deploy files

--- deploy/heroku/recreate.bat
+++ deploy/heroku/recreate.bat
@@ -1,5 +1,5 @@
 CALL heroku apps:destroy bii --confirm bii
 CALL heroku create bii
 CALL git remote add production git@heroku.com:bii.git
-CALL production.bat
+CALL %~dp0\production.bat
 CALL heroku run --app bii python manage.py syncdb
--- deploy/heroku/recreateTest.bat
+++ deploy/heroku/recreateTest.bat
@@ -1,5 +1,5 @@
 CALL heroku apps:destroy bii-test --confirm bii-test
 CALL heroku create bii-test
 CALL git remote add test git@heroku.com:bii-test.git
-CALL test.bat
+CALL %~dp0\test.bat
 CALL heroku run --app bii-test python manage.py syncdb
