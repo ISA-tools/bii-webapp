6b3241a24dd3eaebf1334a71924b74ac78d67a78
Pavlos <raiden4589@gmail.com>
2013-05-01 04:29:52 +0100

    fgh

---
+++ deploy/heroku/production.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Pavlos\Dropbox\Masters\Project\Workspace\Bitbucket\bii
+git checkout heroku
+git merge master
+git push production heroku 
+git checkout master
---
+++ deploy/heroku/test.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Pavlos\Dropbox\Masters\Project\Workspace\Bitbucket\bii_web
+git checkout heroku
+git merge master
+git push test heroku 
+git checkout master
