c89c5469ea8c6bda5d8efccad850b7256a2577d0
Pavlos <raiden4589@gmail.com>
2013-05-01 02:25:26 +0100

    moved pyc files

--- deploy/heroku/recreate.bat
+++ deploy/heroku/recreate.bat
@@ -1,6 +1,3 @@
-CALL cd bii
 CALL heroku apps:destroy bii --confirm bii
 CALL heroku create bii
-CALL ../deploy.bat
-CALL heroku run python manage.py syncdb
-CALL cd ..
+CALL git remote add production git@heroku.com:bii.git
