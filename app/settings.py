e919670bcad920a148d1deaebb146cc72c4073b9
Pavlos Gi <raiden4589@gmail.com>
2013-05-02 02:16:08 +0100

    served static files

--- app/settings.py
+++ app/settings.py
@@ -119,15 +119,12 @@ STATICFILES_FINDERS = (
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-
 # STATIC_ROOT = os.path.join(PROJECT_PATH,"/static/")
-STATIC_ROOT=os.path.join(PROJECT_PATH,'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
 #     PROJECT_PATH+"/static/",
