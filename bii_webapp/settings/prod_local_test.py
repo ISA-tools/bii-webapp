777d110c506bd60bf2e2feb2e300d24db7bfbaae
Pavlos <pavlos@pavlos-VirtualBox.(none)>
2013-05-07 02:31:10 +0100

    added profile edit, fixes for heroku

---
+++ bii_webapp/settings/prod_local_test.py
@@ -0,0 +1,17 @@
+from prod import *
+
+ALLOWED_HOSTS = [".192.168.56.1",".localhost"]
+
+SITE_ID=2
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1','192.168.56.1')
+
+DEBUG=True
