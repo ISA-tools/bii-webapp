83fb3e853b597392d442b1e62276cc1d6c3e7145
Pavlos <pavlos@pavlos-VirtualBox.(none)>
2013-05-16 08:01:22 +0100

    added breadcrumbs requirement created page for #3 ie compatible

--- bii_webapp/urls.py
+++ bii_webapp/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^accounts/', include('bii_webapp.apps.accounts.urls')),
     url(r'^browse/', include('bii_webapp.apps.browse.urls')),
-    url(r'^',RedirectView.as_view(url='/browse/')),
+    url(r'^$',RedirectView.as_view(url='/browse/')),
     url(r'^upload/', include('bii_webapp.apps.fileupload.urls')),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^bii_webapp/static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
