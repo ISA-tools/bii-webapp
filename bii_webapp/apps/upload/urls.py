d9a4d0d1cb58dd03a5ddbc5ae89c618c7cb435f9
pavlos <pavlos@pavlos-VirtualBox.(none)>
2013-05-20 11:38:02 +0100

    changing upload

--- bii_webapp/apps/upload/urls.py
+++ bii_webapp/apps/upload/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.contrib import admin
+from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
     url(r'^$', upload, name='upload.upload'),
-    # (r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), {}, 'upload-delete'),
 )
 
