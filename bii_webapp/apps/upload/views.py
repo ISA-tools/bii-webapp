e68f4b02ebaafdb5690a4cad5edc13da92761b25
Pavlos <raiden4589@gmail.com>
2013-06-03 09:58:14 +0100

    uploading to using actual serverus

--- bii_webapp/apps/upload/views.py
+++ bii_webapp/apps/upload/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
 from django import forms
-import requests
 
 
 def upload(request):
@@ -12,7 +11,6 @@ def upload(request):
             url = settings.WEBSERVICES_URL + 'validate'
             headers = {'content_type': 'form-data'}
             data = {'ZipFile': form.cleaned_data['ZipFile']}
-            r = requests.post(url, data=data, headers=headers)
             x = 1
             return render_to_response("upload.html", context_instance=RequestContext(request))
     else:
